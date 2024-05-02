cb89945009a288de25b4eaf28f7fcd71cadaf453
hafizadt ghazali <hafizadt.ghazali@gmail.com>
2024-05-02 13:14:50 +0800

    Update Timebox.svelte

--- src/components/Timebox.svelte
+++ src/components/Timebox.svelte
@@ -1,15 +1,10 @@
 <script lang="ts">
   import { format } from 'date-fns';
-  import { setLocale, locale } from 'svelte-i18n';
-  import ms from '../locales/ms.json';
 
   export let prayerName: string;
   export let prayerTime: Date;
   export let timeToNextPrayer: string;
   export let isNextPrayer: boolean;
-
-  const t = locale("ms");
-
 
   // Function to translate prayer names to Malay
   function translatePrayerName(prayer: string): string {
@@ -40,7 +35,7 @@
   <div class="waqt-name">
     {#if isNextPrayer}
       {translatePrayerName(prayerName)}
-      <span class="next-waqt-time">{t(timeToNextPrayer ?? '')}</span>
+      <span class="next-waqt-time">{timeToNextPrayer ?? ''}</span>
     {:else}
       {translatePrayerName(prayerName)}
     {/if}
